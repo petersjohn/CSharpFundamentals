b70ace8994dbe064fb7e1318fd24039b706e4f35
John Peters <petersjohn0109@gmail.com>
2021-05-27 22:33:26 -0400

    Methods

---
+++ 04_Methods/MethodExamples.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _04_Methods
+{
+    [TestClass]
+    public class MethodExamples
+    {
+        //input
+        //what we do
+        //output
+
+        //access modifier -- return type --method signature (name and list of parameters
+        public int AddTwoNumbers(int num1,int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+        private int SubtractTwoNumbers(int a, int b)
+        {
+            int num = a - b;
+            return num;
+        }
+
+        private int MultiplyTwoNumbers(int x, int z)
+        {
+            int prod = x * z;
+            return prod;
+        }
+         private int DivideTwoNumbers(int apricot, int cherry)
+        {
+            int fruitSalad = apricot / cherry;
+            return fruitSalad;
+        }
+
+        private int FindRemainder(int a, int numTwo)
+        {
+            int remainder = a % numTwo;
+            return remainder;
+        }
+
+
+        [TestMethod]
+        public void MethodTests()
+        {
+            int banana = AddTwoNumbers(7, 12); //items in the parameters are arguments
+            Assert.AreEqual(19, banana);
+
+            int subtractedBanana = SubtractTwoNumbers(10, 5);
+            Assert.AreEqual(5, subtractedBanana);
+
+            int product = MultiplyTwoNumbers(12, 5);
+            Assert.AreEqual(60, product);
+
+            int fruitSalad = DivideTwoNumbers(10, 4);
+            Assert.AreEqual(2, fruitSalad);
+
+            int remainder = FindRemainder(10, 4);
+            Assert.AreEqual(2, remainder);
+
+        }
+    }
+}
